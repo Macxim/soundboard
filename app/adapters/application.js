c0dbe42a95ded1726df5f847fcd6287d158976e9
Maxime Laforet <maxime10081985@gmail.com>
2014-11-07 16:31:51 +0100

    Test query.Fixtrues (not working)

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,3 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.FixtureAdapter.extend({});
+
+// The following does not work.
+// export default DS.FixtureAdapter.extend({
+//   queryFixtures: function(fixtures, query) {
+//     var key = Ember.keys(query)[0];
+//     return fixtures.filterBy(key, query[key]);
+//   }
+// });
