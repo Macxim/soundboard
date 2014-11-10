497e2359d6d2cd5c4f9dede102e72c31ff232792
Maxime Laforet <maxime10081985@gmail.com>
2014-11-10 09:31:16 +0100

    Links with sound's ID

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,7 @@
-  import Ember from 'ember';
+import Ember from 'ember';
 
-  export default Ember.Route.extend({
-    model: function(params) {
-      return this.store.find('sound', { fileName: params.fileName });
-    },
-    serialize: function(model) {
-      return { fileName: model.fileName };
-    }
-  });
+export default Ember.Route.extend({
+  model: function() {
+    return this.store.find('sound');
+  }
+});
