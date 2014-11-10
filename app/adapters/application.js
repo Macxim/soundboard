31538e273784c5f2b87a5ac7ea7f3e824bc61ac9
Maxime Laforet <maxime10081985@gmail.com>
2014-11-10 09:31:16 +0100

    Links with sound's ID

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,11 +1,3 @@
 import DS from 'ember-data';
 
 export default DS.FixtureAdapter.extend({});
-
-// The following does not work.
-// export default DS.FixtureAdapter.extend({
-//   queryFixtures: function(fixtures, query) {
-//     var key = Ember.keys(query)[0];
-//     return fixtures.filterBy(key, query[key]);
-//   }
-// });
